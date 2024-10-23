eec9695a9b461434c170d2a0fd22876b0843c437
ViRT1ST <ViRT1ST@outlook.com>
2024-10-23 12:16:51 +0500

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import { Open_Sans, Lato, Roboto } from 'next/font/google';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+
+import type { Metadata } from 'next';
+
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Template Site'
+};
+
+const open_sans = Open_Sans({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['300'],
+  display: 'swap',
+  variable: '--font-open-sans',
+});
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['400'],
+  display: 'swap',
+  variable: '--font-lato',
+});
+
+const roboto = Roboto({
+  subsets: ['latin', 'cyrillic'],
+  weight: ['400', '500'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
+const rootLayoutClasses = [
+  open_sans.variable,
+  lato.variable,
+  roboto.variable,
+  GeistSans.variable, 
+  GeistMono.variable,
+];
+
+type Props = Readonly<{
+  children: React.ReactNode;
+}>;
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="en">
+      <body className={`${rootLayoutClasses.join(' ')} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Examples from '@/components/examples';
+
+export default function Home() {
+  return (
+    <div className="mt-8 ml-16 w-96 font-geistsans">
+      <h1 className="mb-8">Main Page</h1>
+      <Examples />
+    </div>
+  );
+}
+
